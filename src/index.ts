05b333c07a8d83fe97e6f2a0dbd3fe6d61012b50
David Heidrich <david.heidrich@freshcells.de>
2025-06-12 15:55:58 +0200

    fix: added `removeIgnoreSetsFromDocument` to the main export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { KeepLink } from './apollo/KeepLink'
+import { KeepLink, removeIgnoreSetsFromDocument } from './apollo/KeepLink'
+
+export { removeIgnoreSetsFromDocument }
 
 export default KeepLink
